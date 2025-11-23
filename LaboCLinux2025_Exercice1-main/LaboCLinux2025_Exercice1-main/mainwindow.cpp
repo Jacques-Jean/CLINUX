503cd3d4700f1a8fe8853e947169ada2284f0b44
jj <jacques-jean.boutros@student.hepl.be>
2025-11-23 16:37:57 +0100

    exercice 1 et 2 ajouté

---
+++ LaboCLinux2025_Exercice1-main/LaboCLinux2025_Exercice1-main/mainwindow.cpp
@@ -0,0 +1,96 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <stdio.h>
+#include <string.h>
+
+typedef struct
+{
+  int   id;
+  char  nom[40];
+  char  email[40];
+} ELEMENT;
+
+ELEMENT Elm[] = 
+  { 
+  {0,"",""},
+  {1,"aaa","aaa@gmail.com"},
+  {2,"bbb","bbb@gmail.com"},
+  {3,"ccc","ccc@gmail.com"},
+  {4,"ddd","ddd@gmail.com"},
+  {5,"eee","eee@gmail.com"},
+  {0,"",""}
+  };
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) , ui(new Ui::MainWindow)
+{
+  ui->setupUi(this);
+  
+  //***** A modifier ***********************
+  static int currentIndex = 1;  // index initial à 1
+  setNom(Elm[currentIndex].nom);
+  setEmail(Elm[currentIndex].email);
+  //****************************************
+}
+
+MainWindow::~MainWindow()
+{
+  delete ui;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///// Fonctions utiles : ne pas modifier /////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void MainWindow::setNom(const char* Text)
+{
+  fprintf(stderr,"---%s---\n",Text);
+  if (strlen(Text) == 0 )
+  {
+    ui->lineEditNom->clear();
+    return;
+  }
+  ui->lineEditNom->setText(Text);
+}
+
+void MainWindow::setEmail(const char* Text)
+{
+  fprintf(stderr,"---%s---\n",Text);
+  if (strlen(Text) == 0 )
+  {
+    ui->lineEditEmail->clear();
+    return;
+  }
+  ui->lineEditEmail->setText(Text);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///// Fonctions clics sur les boutons ////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void MainWindow::on_pushButtonSuivant_clicked()
+{
+  fprintf(stderr,"Clic sur le bouton >>>\n");
+
+  currentIndex++;
+  if (Elm[currentIndex].id == 0) currentIndex = 1;  // revenir au début si fin
+  
+  setNom(Elm[currentIndex].nom);
+  setEmail(Elm[currentIndex].email);
+}
+
+void MainWindow::on_pushButtonPrecedent_clicked()
+{
+  fprintf(stderr,"Clic sur le bouton <<<\n");
+  currentIndex--;
+  if (Elm[currentIndex].id == 0) currentIndex = 5;  // revenir à la fin si début
+  
+  setNom(Elm[currentIndex].nom);
+  setEmail(Elm[currentIndex].email);
+}
+
+void MainWindow::on_pushButtonQuitter_clicked()
+{
+  fprintf(stderr,"Clic sur le bouton Quitter\n");
+  qApp->quit();
+}
---
+++ LaboCLinux2025_Exercice2-main/FichierUtilisateur.cpp
@@ -0,0 +1,106 @@
+#include "FichierUtilisateur.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+int estPresent(const char* nom)
+{
+  int fd;
+  ssize_t n;
+  UTILISATEUR e;
+  fd = open(FICHIER_UTILISATEURS, O_RDONLY);
+  int pos= 0;
+  if (fd == -1)
+    return -1;
+  
+  while (( n = read(fd , &e , sizeof(UTILISATEUR))) > 0){
+    if (strcmp(e.nom ,nom )==0){
+      close(fd);
+      return pos+1;
+        
+    }
+    else pos++;
+  }
+  
+  close(fd);
+  return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+int hash(const char* motDePasse)
+{
+  int i =0;
+  int hash=0;
+  while( (motDePasse[i] != '\0')){
+    hash+= (i+1)*motDePasse[i];
+    i++;
+  }
+  return hash%97;
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+void ajouteUtilisateur(const char* nom, const char* motDePasse)
+{
+  
+  
+  int fd = open(FICHIER_UTILISATEURS, O_WRONLY | O_APPEND | O_CREAT, 0664);
+  if (fd == -1) {
+    return;
+  }
+  
+  UTILISATEUR e;
+  strcpy(e.nom, nom);
+  e.hash = hash(motDePasse);
+  int a = write(fd, &e, sizeof(UTILISATEUR));
+  close(fd);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////
+int verifieMotDePasse(int pos, const char* motDePasse)
+{
+  int fd = open(FICHIER_UTILISATEURS, O_RDONLY);
+  if (fd==-1){
+    return -1;
+  }
+  if (lseek(fd,(pos-1)*sizeof(UTILISATEUR),SEEK_SET)==-1){
+    close(fd);
+    return -1;
+  }
+  UTILISATEUR e;
+  ssize_t n = read(fd , &e , sizeof(UTILISATEUR));
+  
+  close(fd);
+  if(n!= sizeof(UTILISATEUR))
+    return -1;
+  
+  if(e.hash==hash(motDePasse)){
+    
+    return 1;
+  }
+  else
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+int listeUtilisateurs(UTILISATEUR *vecteur) // le vecteur doit etre suffisamment grand
+{
+  int fd;
+  ssize_t n;
+  UTILISATEUR e;
+  fd = open(FICHIER_UTILISATEURS, O_RDONLY);
+  int userNumber= 0;
+  if (fd == -1)
+    return -1;
+  
+  while (( n = read(fd , &e , sizeof(UTILISATEUR))) > 0){
+      
+    vecteur[userNumber]=e;
+    userNumber++;
+      
+    
+  }
+  close(fd);
+  return userNumber;
+}
